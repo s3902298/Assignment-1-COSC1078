7b28f0828b06fb405c9e89513349a4bfa3e40ca8
64-Tesseract <63184095+64-Tesseract@users.noreply.github.com>
2021-03-25 14:26:06 +1100

    Tweaked padding *again*, wrote another script

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ I utterly hate HTML, but, what else can I do?
 var headerElement = document.getElementById("scriptHeader");
 var mainElement = document.getElementById("scriptMain");
 var footerElement = document.getElementById("scriptFooter");
+var buttonArea = document.getElementById("buttonArea");
 var greybackElements = document.getElementsByClassName("main");
 var navElement = document.getElementsByClassName("nav")[0];
 var desktopLayout = null;
@@ -21,10 +22,13 @@ function resizeMain () {
         for (var e = 0; e < 2; e++) {
             greybackElements[e].style.display = (desktopLayout ? "flex" : "block");
             greybackElements[e].style.width = (desktopLayout ? "65%" : "90%");
+            greybackElements[e].style.padding = (desktopLayout ? "0px 10px" : "0px");
             greybackElements[e].style.backgroundColor = (desktopLayout ? "rgba(24, 24, 24, 1)" : "rgba(24, 24, 24, 0)");
         }
         navElement.style.width = (desktopLayout ? "85%" : "100%");
         footerElement.style.width = (desktopLayout ? "65%" : "100%");
+        footerElement.style.padding = (desktopLayout ? "10px" : "0px");
+        buttonArea.style.display = (desktopLayout ? "flex" : "none");
     }
 }
 
@@ -34,6 +38,10 @@ function parallaxBack () {
     var xpos = Math.sin(mainElement.scrollTop / 800) * 10;
     var ypos = -mainElement.scrollTop / 4;
     mainElement.style.backgroundPosition = xpos + "px " + ypos + "px";
+}
+
+function annoyingButton () {
+    console.log("Blah");
 }
 
 
